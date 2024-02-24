f90ca9870d3b1b7deb818c13b5feede46967445f
mikeqiu <mikeqiu2011@163.com>
2024-02-24 15:05:47 +0800

    first trivil test

---
+++ src/test/java/JunitExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JunitExampleTest {
+    @Test
+    public void demoTestMethod(){
+        assertTrue(true);
+    }
+}
