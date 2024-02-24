8704f2d000b5c70cad64de0b309a890845b847bb
mikeqiu <mikeqiu2011@163.com>
2024-02-24 20:21:07 +0800

    getAllRecords_success done

--- src/test/java/com/rest/app/BookControllerTest.java
+++ src/test/java/com/rest/app/BookControllerTest.java
@@ -1,7 +1,62 @@
 package com.rest.app;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(MockitoJUnitRunner.class)
 class BookControllerTest {
+    private MockMvc mockMvc;
 
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    ObjectWriter objectWriter = objectMapper.writer();
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookController bookController;
+
+    Book RECORD_1 = new Book(1L, "Habits", "how to build better habits", 5);
+    Book RECORD_2 = new Book(2L, "Thinking", "how to create good mental model", 4);
+    Book RECORD_3 = new Book(3L, "algorithms", "learning alg in the fun way", 5);
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+    }
+
+    @Test
+    void getAllRecords_success() throws Exception {
+        List<Book> records = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2, RECORD_3));
+
+        Mockito.when(bookRepository.findAll()).thenReturn(records);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/book")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[2].name", is("algorithms")));
+    }
 }
